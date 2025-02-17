ee92e4eb72307fca8eaf9180296b0b32b4969bf0
Raj Kumar Paswan <34989041+pasraj@users.noreply.github.com>
2025-02-17 20:42:31 +0530

    quick sort added for the partition logic

--- quicksort.py
+++ quicksort.py
@@ -9,6 +9,31 @@ def quick_sortv1(arr):
     return quick_sortv1(left) + middle + quick_sortv1(right)
 
 
+def partitionv2(arr, low, high):
+    pivot = arr[high] # choosing last element as pivot
+    i = low-1 # pointer for the smaller element 
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i+=1
+            arr[i], arr[j] =arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1 # return the pivot point
+
+def quick_sort_inplacev2(arr, low, high):
+    if low < high:
+        pi = partitionv2(arr, low, high)
+        quick_sort_inplacev2(arr, low, pi-1)
+        quick_sort_inplacev2(arr,pi + 1, high)  
+
+
+
+arr = [10, 7, 8, 9, 1, 5]
+quick_sort_inplacev2(arr, 0, len(arr) - 1)
+print(arr)
+
+
 unsorted_list = [4,4,2,6,2,673,2,4,24]
 sorted_list = quick_sortv1(unsorted_list)
 print(sorted_list)
